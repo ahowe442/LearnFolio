63369dfbd808ba6102b088016e325bf873debc66
Abby H <howe.abigail@gmail.com>
2020-04-10 20:07:49 -0500

    removed .gitignore because it was causing errors.

---
+++ src/main/java/com/abbyhowe/LearnFolio/LearnFolioAppApplication.java
@@ -0,0 +1,13 @@
+package com.abbyhowe.LearnFolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnFolioAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnFolioAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abbyhowe/LearnFolio/LearnFolioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abbyhowe.LearnFolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnFolioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
