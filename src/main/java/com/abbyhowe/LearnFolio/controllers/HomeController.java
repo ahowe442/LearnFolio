4eee804f44f44e65a44f4319915a084572a3190b
Abby H <howe.abigail@gmail.com>
2020-04-12 19:40:41 -0500

    removed sample HelloController.java. Added HomeController with demo code. Added bootstrap.js and learnfolio.css styling.

--- src/main/java/com/abbyhowe/LearnFolio/controllers/HomeController.java
+++ src/main/java/com/abbyhowe/LearnFolio/controllers/HomeController.java
@@ -1,16 +1,18 @@
 package com.abbyhowe.LearnFolio.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
+public class HomeController {
 
     @GetMapping
     @ResponseBody
     public String hello(){
         return "Hello, Spring!";
     }
+
 
 }
