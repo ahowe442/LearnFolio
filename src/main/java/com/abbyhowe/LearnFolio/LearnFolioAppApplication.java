99f21f1152b2a1777ae7fac2e10f558bcf3e105d
Abby H <howe.abigail@gmail.com>
2020-05-06 16:40:00 -0500

    adds file upload

--- src/main/java/com/abbyhowe/LearnFolio/LearnFolioAppApplication.java
+++ src/main/java/com/abbyhowe/LearnFolio/LearnFolioAppApplication.java
@@ -2,10 +2,16 @@ package com.abbyhowe.LearnFolio;
 
 import com.abbyhowe.LearnFolio.models.Student;
 import com.abbyhowe.LearnFolio.models.UserType;
+import com.abbyhowe.LearnFolio.storage.StorageProperties;
+import com.abbyhowe.LearnFolio.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class LearnFolioAppApplication {
 
 	public static void main(String[] args) {
@@ -16,6 +22,14 @@ public class LearnFolioAppApplication {
 		Student U3 = new Student ("John", "Cardella", "John.card@gmail.com", UserType.STUDENT);
 		Student U4 = new Student ("Megan", "Ewy", "ME@gamil.com", UserType.STUDENT);
 		Student U5 = new Student ("Jennifer", "Stowbart", "JS@gmail.com", UserType.STUDENT);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 
---
+++ src/main/java/com/abbyhowe/LearnFolio/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package com.abbyhowe.LearnFolio.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
